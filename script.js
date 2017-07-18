6ef209cdac2b436f496572f0b958cb330a446b98
Sharon Moskal <sharonmoskal@icloud.com>
2017-07-18 09:44:46 -0400

    added baffle

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 $(document).ready(() => {
         console.log('script loaded');
+         var inProgress = false
+$(window).scroll(function() {
+  if (!inProgress) {
+    inProgress = true;
+    $('.baffle').each(function(i) {
+      (function() {
+        var Baffle;
+        Baffle = function() {
+          function Baffle() {
+            var $baffle, b;
+            $baffle = $('.baffle');
+            b = baffle('.baffle', {
+              characters: '+-\u2022~\u2591\u2588\u2593 \u2593\u2592\u2591!=*',
+              speed: 100
+            });
+            $baffle.addClass('is-started');
+            b.start();
+            b.reveal(2000);
+            setTimeout(()=>inProgress = false,2500)
+          }
+          return Baffle;
+        }();
+        $(function() {
+          return new Baffle();
+        });
+      }.call(this));
+    });
+  } else {
+    console.log('throttled');
+  }
+});
+
+
+
+
     }) // ends document.ready
