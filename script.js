0ab14cea5d9ce7b7c3ada0824fa33b464f786b76
Sharon Moskal <sharonmoskal@icloud.com>
2018-05-03 18:04:59 -0400

    updated site

--- script.js
+++ script.js
@@ -1,38 +1,26 @@
 $(document).ready(() => {
         console.log('script loaded');
-         var inProgress = false
-$(window).scroll(function() {
-  if (!inProgress) {
-    inProgress = true;
-    $('.baffle').each(function(i) {
-      (function() {
-        var Baffle;
-        Baffle = function() {
-          function Baffle() {
-            var $baffle, b;
-            $baffle = $('.baffle');
-            b = baffle('.baffle', {
-              characters: '+-\u2022~\u2591\u2588\u2593 \u2593\u2592\u2591!=*',
-              speed: 100
-            });
-            $baffle.addClass('is-started');
-            b.start();
-            b.reveal(2000);
-            setTimeout(()=>inProgress = false,2500)
-          }
-          return Baffle;
-        }();
-        $(function() {
-          return new Baffle();
-        });
-      }.call(this));
-    });
-  } else {
-    console.log('throttled');
-  }
-});
+
+
+	var $aboutHover = $(".projects-sub--about-me")
+	var $about = $(".container--text");
+	var $paragraph = $(".paragraph");
+
+	$aboutHover.hover(function(){
+		$paragraph.addClass("type");
+
+	$(".type").typed({
+		strings:[
+		"Web developer with an emphasis on fashion and jewelry brands. I use a variety of technologies including Javascript, Jquery, React, Node/Express and Sass."
+		],
+		typeSpeed: 80
+	})
+		
+	})
 
 
 
 
-    }) // ends document.ready
+
+
+ }) // ends document.ready
